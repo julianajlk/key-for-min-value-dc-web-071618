67dcc9974c3544aa64a4751b820d08d43899be37
Juliana Kang <julianajlk@gmail.com>
2018-07-12 03:43:03 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -10,5 +10,8 @@ def key_for_min_value(name_hash)
         min_value = name_hash[key]
       end
   end
+  name_hash.each do |key, value|
+    if name_hash[key] == min_value
+      return key
 end
 end
