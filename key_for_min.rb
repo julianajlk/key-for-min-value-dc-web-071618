41d995c14404453a690fee473e888528fd696144
Juliana Kang <julianajlk@gmail.com>
2018-07-12 03:50:32 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, value|
       if min_value == nil
         min_value = key
-      else value.to_i < name_hash[min_value].to_i
+      elsif value.to_i < name_hash[min_value].to_i
         min_value = key
       end
     end
